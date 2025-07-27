ee39bbc07b33522ccffdda3ddf76d174143f941d
Sagolsa78 <sahanimohit5ed@gmail.com>
2025-07-27 23:18:52 +0530

    cors fix try4 url checked.

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,30 @@
 const express=require("express")
 const cors=require("cors");
 const rootRouter=require("./Routes/index")
+const dotenv= require("dotenv");
+
 
 const app=express();
 
 app.use(cors({
-    origin:"https://payme-sagolsa78.vercel.app/",
+    origin:"https://payme-sagolsa78.vercel.app",
     credentials: true,
-    methods:["GET", "POST"]
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+
 }));
+
+app.options("*",cors());
+
+
 
 app.use(express.json());
 app.use("/api/v1",rootRouter);
 
+ const PORT=process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}`);
+    
+});
 
